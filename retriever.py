ec3966d8ad910fa107cc8bf6d7752c063a18e704
Ashok Koduru <ashokvsk@ccs.neu.eduu>
2016-12-06 18:54:18 -0500

    Reading Queries, TF IDF, Cosine model

--- retriever.py
+++ retriever.py
@@ -6,53 +6,86 @@
 
 from indexer import Indexer
 from collections import Counter
+from bs4 import BeautifulSoup
 import os
 import glob
 import operator
 import math
+import re
 
 
 class Retriever:
 
     def __init__(self):
-        self.query = ''
-        self.ind = Indexer()
-        return
+        index = self.build_inverted_index()
+        self.inverted_index = index[0]
+        self.total_corpus = index[1]
+        self.token_count = index[2]
 
-    def rank_docs_by_query(self, query=''):
-        self.query = raw_input("Enter the search query\n") if query == '' else query
-        query_terms = self.query.lower().split()
+    def run_query(self, query, queryid):
+        print "running query : " + query
+        ranked_docs = self.cosine_similarity(query)
+        fname = '%s.txt' % query
+        os.chdir('..')
+        f = open(fname, 'w')
+        for each in ranked_docs:
+            f.write('{} {} {} {} {} {}\n'.format(queryid, 'Q0',
+                                                 each[0],
+                                                 ranked_docs.index(each)+1, each[1], 'system_name'))
+        f.close()
+
+    def build_inverted_index(self):
+        self.navigate_to_docs()
+        inverted_index = {}
+        corpus_content = {}
+        token_count = {}
+        for eachfile in glob.glob('*.html'):
+            docid = eachfile[:-5]
+            content = open(eachfile).read()
+            parsed_content = self.parse_document(content)
+            content_as_list = parsed_content.split()
+            content_as_list = [s.encode("UTF-8") for s in content_as_list]
+            corpus_content[docid] = content_as_list
+            token_count[docid] = len(content_as_list)
+            word_count = dict(Counter(content_as_list))
+            for token in content_as_list:
+                if token not in inverted_index:
+                    temp = dict()
+                    temp[docid] = word_count[token]
+                    inverted_index[token] = temp
+                else:
+                    temp = inverted_index[token]
+                    temp[docid] = word_count[token]
+                    inverted_index[token] = temp
+        return inverted_index, corpus_content, token_count
+
+    def cosine_similarity(self, query):
+        query_terms = query.lower().split()
         query_dict = dict(Counter(query_terms))
         m_query = 0
         for each in query_dict:
             m_query += math.pow(query_dict[each], 2)
 
         mag_query = math.sqrt(m_query)
-        inv_index = self.ind.build_n_gram_index(1)
-        cosine_sim = {}
-        os.chdir('..')
-        ind = Indexer()
-        doc_dict_id = ind.build_docid_dict(ret=True)
-        parsed_corpus = os.path.join(os.getcwd(), 'parsed_corpus')
-        os.chdir(parsed_corpus)
+        inv_index = self.inverted_index
+        total_corpus = self.total_corpus
+        cosine_similarity = {}
         docset = []
         for each in query_terms:
             doclist = inv_index[each]
             for d in doclist:
                 docset.append(d)
         docset = list(set(docset))
-        for eachfile in glob.glob('*.txt'):
-            fname = eachfile[:len(eachfile) - 4]
-            if doc_dict_id[fname] not in docset:
+        for eachfile in total_corpus:
+            docid = eachfile[:-5]
+            if docid not in docset:
                 continue
-            file_content = open(eachfile)
-            content = file_content.read()
-            content = content.split()
+            content = total_corpus[docid]
             word_count = dict(Counter(content))
             sum_num = 0
             m_doc = 0
             for each in word_count:
-                idf_comp = 1 + math.log(float(989)/len(inv_index[each]))
+                idf_comp = 1 + math.log(float(len(total_corpus))/len(inv_index[each]))
                 tf_word = word_count[each]/float(len(content))
                 m_doc += math.pow(tf_word*idf_comp, 2)
             mag_doc = math.sqrt(m_doc)
@@ -60,32 +93,55 @@ class Retriever:
             for each_query_term in query_terms:
                 if each_query_term in content:
                     tf = word_count[each_query_term]/float(len(content))
-                    idf = 1 + math.log(float(989)/len(inv_index[each_query_term]))
+                    idf = 1 + math.log(float(len(total_corpus))/len(inv_index[each_query_term]))
                     x = tf*idf
-                    sum_num += x*query_terms.count(each_query_term)
+                    sum_num += x*query_terms.count(each_query_term)*1
                 else:
                     continue
             denominator = mag_query*mag_doc
             similarity = sum_num/denominator
-            cosine_sim[fname] = similarity
-        sorted_cosine_dict = sorted(cosine_sim.items(), key=operator.itemgetter(1), reverse=True)
-        sorted_cosine_dict = sorted_cosine_dict[:100]
-        return sorted_cosine_dict
+            cosine_similarity[docid] = similarity
+        # sorted_cosine_dict = sorted(cosine_sim.items(), key=operator.itemgetter(1), reverse=True)
+        # sorted_cosine_dict = sorted_cosine_dict[:100]
+        return cosine_similarity
 
-    def run_query(self, query, queryid):
-        print "running query : " + query
-        ranked_docs = self.rank_docs_by_query(query)
-        os.chdir('..')
-        ind = Indexer()
-        doc_dict_id = ind.build_docid_dict(ret=True)
-        fname = '%s.txt' % query
-        f = open(fname, 'w')
-        for each in ranked_docs:
-            f.write('{} {} {} {} {} {}\n'.format(queryid, 'Q0',
-                                                 doc_dict_id[each[0]],
-                                                 # each[0],
-                                                 ranked_docs.index(each)+1, each[1], 'system_name'))
-        f.close()
+
+
+    def tf_idf(self, query):
+        query_terms = query.split()
+        corpus_content = self.total_corpus
+        tf_idf_scores = {}
+        total_files = len(corpus_content)
+        for eachfile in corpus_content:
+            docid = eachfile[:-5]
+            tf_idf_file = 0
+            for each_query_term in query_terms:
+                tf_term = corpus_content[eachfile].count(each_query_term)
+                idf_term = 1 + math.log(float(total_files)/len(self.inverted_index[eachfile]))
+                tf_idf_file += tf_term * idf_term
+            tf_idf_scores[docid] = tf_idf_file
+
+        return tf_idf_scores
+
+    def navigate_to_docs(self):
+        cwd = os.getcwd()
+        cacm = os.path.join(cwd, 'cacm')
+        os.chdir(cacm)
+
+    def read_queries(self):
+        with open("cacm.query") as content:
+            regex = re.compile(r'<DOCNO>\s+(.*?)\s+</DOCNO>(.*?)</DOC>', re.DOTALL)
+            queries = re.findall(regex, content.read().replace("\n", ''))
+
+        query_file = open('cacm_query.query', 'w')
+        for query in queries:
+            query_file.write('{} {}\n'.format(query[0], query[1][1:-1]))
+
+    def parse_document(self, file_content):
+        content = BeautifulSoup(file_content, 'html.parser')
+        content = content.get_text()
+        return content
+
 
     def modify_lucene_files(self):
         ind = Indexer()
@@ -129,24 +185,15 @@ class Retriever:
 
 
 def hw4_tasks():
+
     r = Retriever()
-    queries = dict()
-    queries['global warming potential'] = 1
-    queries['green power renewable energy'] = 2
-    queries['solar energy california'] = 3
-    queries['light bulb bulbs alternative alternatives'] = 4
-    live = True
-    print "Welcome to search\n"
-    while live:
-        query = raw_input('\n\nEnter the query (type \'exit\' to end and \'hw4\' to run the assignment)\n')
-        if query == 'exit':
-            break
-        elif query == 'hw4':
-            for each in queries:
-                r.run_query(each, queries[each])
-        else:
-            query_id = queries[query] if query in queries else 0
-            r.run_query(query, query_id)
+    # print r.inverted_index['the']
+    print r.total_corpus['CACM-0001']
+    # r.read_queries()
+    # r.build_inverted_index()
+
+
+    # r.run_query(query, query_id)
     # r.modify_lucene_files()
     # r.merge_files('Ranked_Docs')
     return
